7f703ab31288928d2de62eebeedb866656c8e516
niooii <wangh3@bxscience.edu>
2023-12-12 17:19:57 -0500

    r

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/niooi/Projects/Cpp/vulkan_stuff
-# Build directory: /home/niooi/Projects/Cpp/vulkan_stuff/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
